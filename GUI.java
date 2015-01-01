c67e7cfdbadbe31d590d62a820aae7be337f7b59
blehw <winton2095@gmail.com>
2015-01-01 17:07:36 -0500

    loaded map

--- GUI.java
+++ GUI.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import javax.swing.*;
 import java.awt.*;
 
@@ -5,24 +6,33 @@ public class GUI extends JFrame {
 
     private Container pane;
     private JLabel l;
-    private JPanel canvas; 
 
     public GUI() {
 
 	setTitle("Pacman");
-	setSize(400,400);
+	setSize(800,800);
 	setLocation(100,100);
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	pane = getContentPane();
 	pane.setLayout(new FlowLayout());
+
 	l = new JLabel("Pacman");
 	pane.add(l);
 
+        ImageIcon image = new ImageIcon("images/level1.png");
+	JLabel label = new JLabel("", image, JLabel.CENTER);
+	JPanel panel = new JPanel(new BorderLayout());
+	panel.add( label, BorderLayout.CENTER );
+	pane.add(panel);
+	
+
+	/*
 	canvas = new JPanel();
 	canvas.setPreferredSize(new Dimension(300,300));
 	canvas.setBorder(BorderFactory.createLineBorder(Color.black,2));
 	pane.add(canvas);
+	*/
     }
 
     public static void main(String[] args) {
