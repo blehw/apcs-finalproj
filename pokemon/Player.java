097c72afeada7a6a9ba9d2cb9d58037540f2b932
Winton Yee <wyee@github.com>
2015-01-16 07:58:24 -0500

    commited Driver

--- pokemon/Player.java
+++ pokemon/Player.java
@@ -39,37 +39,11 @@ public class Player {
 	}
 	return s;
     }
-    
-
-    public BaseChar getPokemon0(){
-	return pokemon[0];
-    }
-
-    public BaseChar getPokemon1(){
-	return pokemon[1];
-    }
-
-    public BaseChar getPokemon2(){
-	return pokemon[2];
-    }
-
-    public BaseChar getPokemon3(){
-	return pokemon[3];
-    }
-
-    public BaseChar getPokemon4(){
-	return pokemon[4];
-    }
-    
-    public BaseChar getPokemon5(){
-	return pokemon[5];
-    }
 
     public BaseChar[] getPokemon(){
 	return pokemon;
     }
     
-
     public String[] getBadges(){
 	return badges;
     }
@@ -86,53 +60,16 @@ public class Player {
 	return s;
     }
 
-    
     public void setLocation(int loc){
 	location = loc;
     }
-
-
-    public void setPokemon0(BaseChar poke){
-	pokemon[0] = poke;
+    
+    public void setPokemon(BaseChar poke, int pos){
+	pokemon[pos] = poke;
     }
 
-    public void setPokemon1(BaseChar poke){
-	pokemon[1] = poke;
+    public void setBadges(String badgeName, int pos){
+	badges[pos] = badgeName;
     }
-
-    public void setPokemon2(BaseChar poke){
-	pokemon[2] = poke;
-    }
-
-    public void setPokemon3(BaseChar poke){
-	pokemon[3] = poke;
-    }
-
-    public void setPokemon4(BaseChar poke){
-	pokemon[4] = poke;
-    }
-
-    public void setPokemon5(BaseChar poke){
-	pokemon[5] = poke;
-    }
-
-
-
-    public void setBadges0(String badgeName){
-	badges[0] = badgeName;
-    }
-
-    public void setBadges1(String badgeName){
-	badges[1] = badgeName;
-    }
-
-    public void setBadges2(String badgeName){
-	badges[2] = badgeName;
-    }
-
-    public void setBadges3(String badgeName){
-	badges[3] = badgeName;
-    }
-
 
 }
